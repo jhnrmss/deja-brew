e216fb1d73adaa48a1860c0bbd501b0318e9a6c1
John Romie <88918248+jhnrmss@users.noreply.github.com>
2024-03-07 19:53:30 +0800

    chore/add components

---
+++ src/components/index.tsx
@@ -0,0 +1,22 @@
+export { Button, buttonVariants } from "./AppButton/button";
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./AppCard/card";
+export { Tabs, TabsList, TabsTrigger, TabsContent } from "./AppTabs/tabs";
+export { Input as InputBox } from "./AppInput/input";
+export { Label } from "./AppLabel/label";
+export {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from "./AppForm/form";
